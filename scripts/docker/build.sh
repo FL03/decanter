48dae1135a2afc821f360e3df21ae96388a5e967
Joe McCain III <jo3mccain@scattered-systems.com>
2022-09-29 15:43:27 +0000

    Update

--- scripts/docker/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/cargo/env bash
-
-docker build . --tag jo3mccain/crypto-automata
--- scripts/docker/latest.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/cargo/env bash
-
-docker run -d -rm --name flow -p 9876:9876 jo3mccain/crypto-automata:latest
